8f440f38b976e9c0e241fcf041c902d171dbbe50
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-09 11:53:32 +0530

    updated

--- Js Functions/return.js
+++ Js Functions/return.js
@@ -33,3 +33,30 @@ function greet(name){
     return console.log("welcome to js functions " + name)
 }  
 greet("chalu")
+
+
+//example 5
+
+function add(x,y){
+    var res = x+y
+    return res
+}
+var res = add(100,200)
+console.log(res)
+
+
+//example 6
+
+function fun(){
+    return "welcome to js functions"
+}
+console.log(fun())
+
+
+//example 7
+
+function fun(){
+    return "welcome to js functions"
+    console.log("hello")
+}
+console.log(fun())
