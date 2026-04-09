aa003137c70866cd201bde09660149630be9e306
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-09 11:26:03 +0530

    updated

---
+++ Js Functions/parameterized.js
@@ -0,0 +1,12 @@
+//          syntax of parameterized function
+
+//          function functionName(parameters){
+//              //set of code
+//          }
+//          functionName(arguments) //function call
+
+function add(a,b){
+    let res = a+b
+    console.log(res)
+}
+add(100,200)
