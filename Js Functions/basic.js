f94bc3d361da77638535dafa9e2f2cb9a4419306
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-09 11:03:24 +0530

    updated

---
+++ Js Functions/basic.js
@@ -0,0 +1,20 @@
+//syntax of function
+
+// function functionName(){
+//     //function body
+// }
+// functionName() //function call
+
+
+function greet(){
+    console.log("welcome to JS class")
+}
+greet()
+greet()
+function add(){
+    let a=100
+    let b=200
+    let res = a+b
+    console.log(res)
+}
+add()
