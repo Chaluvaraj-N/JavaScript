0582134b734363659a07a4f6ee763e58ba949920
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-10 11:13:21 +0530

    updated

--- Js Functions/Callback&higher.js
+++ Js Functions/Callback&higher.js
@@ -1,8 +1,36 @@
 // example 1
+
 function print(){
     console.log("Hello World");
 }
+print() 
+
+
 function add(x,y){
 let z=x+y
 console.log(z);
 }
+add(10,20)
+
+
+//example 2
+
+function greet(){
+    return "good morning"
+}  
+greet()
+console.log(greet())
+
+
+function fun(){
+    console.log("i am anynomous function")
+}
+fun()
+
+
+//example 3
+
+function fun(f){
+    console.log(f)
+}
+fun("i am anynomous function")
