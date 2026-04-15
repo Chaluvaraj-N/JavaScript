aedb6894f5d4cdf4743f30b6bfb8d7aa3183d4df
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-15 11:02:27 +0530

    updated

---
+++ JavaScript Objects/function_inside_object.js
@@ -0,0 +1,20 @@
+var person={
+    name:"Virat",
+    age:22,
+    height:"male",
+    skill: ['React','Js','css','htm'],
+    address: {
+        state: "karnataka",
+        city: "Bengaluru"
+    },
+eat: function(){
+    console.log(this.name +" is eating")
+}
+}
+console.log(person)
+console.log(person.age)
+console.log(person.skill[1])
+console.log(person.address.city)
+console.log(person.eat)
+person.eat()
+// person.play()
