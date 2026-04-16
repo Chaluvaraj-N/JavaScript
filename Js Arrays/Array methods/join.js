ca00d2ae93c9b960d3f1019b363e8d2be9a561e6
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-16 11:54:01 +0530

    updated

---
+++ Js Arrays/Array methods/join.js
@@ -0,0 +1,4 @@
+var arr6=[10,"sachin",true,15.679,"virat","y"]
+console.log(arr6.join())
+console.log(arr6.join(" "))
+console.log(arr6.join("$"))
---
+++ Js Arrays/Array methods/lastindexof.js
@@ -0,0 +1,2 @@
+var arr5=["raj",100,"rahul","sachin",true,100,99]
+console.log(arr5.lastIndexOf(1000))
