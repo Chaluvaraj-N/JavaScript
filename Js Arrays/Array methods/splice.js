e2ee3259227f825ccba2beedc1c58fb20a6bc1fe
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-16 12:11:07 +0530

    updated

---
+++ Js Arrays/Array methods/splice.js
@@ -0,0 +1,5 @@
+var arr10=[1,2,3,4,5,6,7,8,9]
+console.log("original array before splicing" + arr10)
+var spliceArr=arr10.splice(2,4)
+console.log("spliced Array" + spliceArr)
+console.log("original array after splicing" + arr10)
