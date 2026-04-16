a1a87c48564ed93ea84842c96b7ee7807a5f8e27
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-16 11:26:28 +0530

    updated

---
+++ Js Arrays/Array methods/push.js
@@ -0,0 +1,15 @@
+let arr = []
+console.log(arr)
+arr.push(18)
+arr.push("virat")
+arr.push(true)
+arr.push({age:40, city:"delhi"})
+console.log(arr)
+
+var arr1 = []
+arr.push(10)
+console.log(arr)
+arr.push("virat")
+arr.push(true)
+arr.push(333)
+arr.push(arr)
