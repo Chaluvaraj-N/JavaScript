8d7ab038b49e179166c8600ad01ebba5e8f0d702
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-16 11:29:54 +0530

    updated

---
+++ Js Arrays/Array methods/pop.js
@@ -0,0 +1,14 @@
+var arr1 = [10, "sachin",true,15.78987,"virat","y"]
+console.log(arr1)
+arr1.pop()
+console.log(arr1)
+arr1.pop()
+arr1.pop()
+console.log(arr1)
+
+
+let arr2 = ["true","raj",1000,"y",false]
+console.log(arr2)
+arr2.pop()
+arr2.pop()
+console(arr2)
