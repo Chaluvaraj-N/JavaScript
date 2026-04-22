e8ae87edab85f0ee0e2446bac59c088777e808a9
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-22 11:21:56 +0530

    updated

---
+++ Js Strings/String Methods/replace().js
@@ -0,0 +1,4 @@
+var str = "hello world"
+console.log(str.replace("world", "everyone")) // hello everyone
+console.log(str.replace(/o/g, "0")) // hell0 w0rld  
+console.log(str.replace(/l/g, "1")) // he11o wor1d
