d7b00a8cb89d098e6a6efdaa0b0ca6cbfc7d07c8
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-22 11:02:24 +0530

    updated

--- Js Strings/concatenation.js
+++ Js Strings/concatenation.js
@@ -2,5 +2,5 @@ let fname = "John"
 let lname = "Doe"
 
 // Concatenation using the + operator
-let fullName = fname + lname
+let fullName = fname +" "+ lname
 console.log(fullName) // Output: John Doe
