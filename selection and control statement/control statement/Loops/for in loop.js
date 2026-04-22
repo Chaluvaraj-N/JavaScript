757ced82e56e1c5caf4d03850cec73ed78594752
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-22 12:22:51 +0530

    updated

---
+++ selection and control statement/control statement/Loops/for in loop.js
@@ -0,0 +1,10 @@
+var obj={
+    name:"sachin",
+    id:123,
+    city:"pune",
+    email:"chaluva19@gmail.com"
+}
+for(var x in obj){
+    console.log(x) //only key names
+    //console.log(obj[x]) //only values
+}
