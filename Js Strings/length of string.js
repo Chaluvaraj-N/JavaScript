8465aec659e8f4acd76643aa91d69ff75581e56d
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-22 10:49:24 +0530

    updated

--- Js Strings/length of string.js
+++ Js Strings/length of string.js
@@ -1,4 +1,4 @@
 let city = "bengaluru"
 // Length of the string
 console.log(city.length) // Output: 10
-console.log(city.length - 1) 
+console.log(city.length - 1) // Output: 9 (index of the last character)
