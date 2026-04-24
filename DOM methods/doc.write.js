274d24315586875691c626e3bb1886569d99d9c0
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-24 10:59:50 +0530

    updated

---
+++ DOM methods/doc.write.js
@@ -0,0 +1,6 @@
+// Example of document.write() in JavaScript
+document.write('<h1>Hello from document.write()</h1>');
+document.write('<p>This content is written directly into the page.</p>');
+
+const language = 'JavaScript';
+document.write(`<p>Welcome to ${language}!</p>`);
