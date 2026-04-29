94d790dcea4e7a4389e1845952b4d77e860441a9
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-29 09:06:16 +0530

    updated

--- math_object.js
+++ math_object.js
@@ -9,3 +9,13 @@ console.log(Math.trunc(4.6)) // floor it to nearest integer value and remove the
 console.log(Math.sign(-5)) // if the number is positive it will return 1, if the number is negative it will return -1 and if the number is 0 it will return 0
 console.log(Math.sign(1773)) // print the number is negative or positive
 console.log(Math.random()) // print the random number
+
+
+//generate random number between 1 to 6
+console.log(Math.floor(Math.random() * 6) + 1) 
+
+//generate random number between 1 to 100
+console.log(Math.floor(Math.random() * 100) + 1)
+
+//generate random number between 1 to 50 using trunc
+console.log(Math.trunc(Math.random() * 50) + 1)
