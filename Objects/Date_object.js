e842b52c7b0d446fa56a326e43ee9e9e2b2d9acf
Chaluvaraj <chaluvashetty23@gmail.com>
2026-04-29 09:15:31 +0530

    updated

---
+++ Objects/Date_object.js
@@ -0,0 +1,10 @@
+//when ever we need to know about the date we should use date object gives information regarding Day, Month, year, minutes, Hours, Seconds, and Time Zone
+
+// month --->0----> January, 1----> February, 2----> March, 3----> April, 4----> May, 5----> June, 6----> July, 7----> August, 8----> September, 9----> October, 10----> November, 11----> December
+
+// weekday ---> 0----> Sunday, 1----> Monday, 2----> Tuesday, 3----> Wednesday, 4----> Thursday, 5----> Friday, 6----> Saturday
+
+// date object is used to work with date and time in JavaScript. It provides various methods to get and set the date and time, as well as to perform operations on them.
+
+var date = new Date() // create a new date object with the current date and time
+console.log(date) // print the current date and time
